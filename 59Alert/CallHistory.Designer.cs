c74821aecb669f899b06a22ed8d882e4b1ff2339
Michał Masny <mich.masny@gmail.com>
2016-02-21 00:13:33 +0100

    read log file and create datagrid

---
+++ 59Alert/CallHistory.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace _59Alert
+{
+    partial class CallHistory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CallHistory));
+            this.dgvCallHistory = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCallHistory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCallHistory
+            // 
+            this.dgvCallHistory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCallHistory.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvCallHistory.Location = new System.Drawing.Point(0, 0);
+            this.dgvCallHistory.Name = "dgvCallHistory";
+            this.dgvCallHistory.Size = new System.Drawing.Size(334, 262);
+            this.dgvCallHistory.TabIndex = 0;
+            this.dgvCallHistory.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCallHistory_CellContentClick);
+            // 
+            // CallHistory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 262);
+            this.Controls.Add(this.dgvCallHistory);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CallHistory";
+            this.Text = "Call History";
+            this.Load += new System.EventHandler(this.CallHistory_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCallHistory)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCallHistory;
+    }
+}
+
---
+++ 59Alert/CallHistory.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace _59Alert
+{
+    public partial class CallHistory : Form
+    {
+        public List<CallItem> CallList;
+        public string FolderPath;
+        public const string FolderPathEnding = @"\Five9\Logs\env001.atl.five9.com";
+        public const string LogFileName = "adapter.log";
+        public string LogFilePath;
+        public CallHistory()
+        {
+            InitializeComponent();
+            CallList = new List<CallItem>();
+            FolderPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + FolderPathEnding;
+            LogFilePath = FolderPath + "\\" + LogFileName;
+            FileStream LogStream = new FileStream(LogFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            StreamReader LogStreamReader = new StreamReader(LogStream);
+            string line = "";
+            string time = "";
+            string previousnumber = "";
+            string number = "";
+            string queue = "";
+            while(!LogStreamReader.EndOfStream)
+            {
+                line = LogStreamReader.ReadLine();
+                if (line.Contains("RINGING") && line.Contains("Sending to model"))
+                {
+                    Match regexnumber = Regex.Match(line, @"number"":""(\d)+");
+                    previousnumber = number;
+                    number = regexnumber.Value.Remove(0,9);
+                    time = line.Substring(0, (line.IndexOf(",")));
+                    Match regexqueue = Regex.Match(line, @"campaignName"":""[^""]+");
+                    queue = regexqueue.Value.Remove(0,15);
+                    CallItem callitem = new CallItem(time, number, queue);
+                    if (!String.IsNullOrEmpty(previousnumber) && number == previousnumber)
+                        CallList.RemoveAt(CallList.Count - 1);
+                    CallList.Add(callitem);
+                }
+            }
+            CallList.Reverse();
+        }
+
+        private void dgvCallHistory_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void CallHistory_Load(object sender, EventArgs e)
+        {
+            dgvCallHistory.AutoGenerateColumns = true;
+            dgvCallHistory.DataSource = CallList;
+            dgvCallHistory.AutoResizeColumns();
+        }
+    }
+
+    public class CallItem
+    {
+        public string Time { get; set; }
+        public string Number { get; set; }
+        public string Queue { get; set; }
+
+        public CallItem(string time, string number, string queue)
+        {
+            Time = time;
+            Number = number;
+            Queue = queue;
+        }
+    }
+}
